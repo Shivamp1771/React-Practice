5ab27ae9d4141982c7e764421ea295ca9742781e
unknown <shivamsparekh05@gmail.com>
2023-05-24 20:19:20 -0400

    loading data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import axios from "axios";
+const { useEffect, useState } = React;
 
-function App() {
+interface UserName {
+  fisrt: string;
+  last: string;
+  title: string;
+
+}
+interface UserPicture {
+  thumbnail: string;
+}
+interface UserInfo {
+  name: UserName;
+  picture: UserPicture;
+}
+const fetchapi = (pageNumber: number = 1) => {
+  return axios.get(`https://randomuser.me/api?page=${pageNumber}`)
+    .then(({ data }) => {
+      // console.log(res);
+      return data;
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
+
+const getFullUsername = (userInfo: UserInfo) => {
+  const { name: { first, last } } = userInfo;
+  return `${first} ${last}`;
+
+}
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [nextPageNumber, setNextPageNumber] = useState(1);
+  const [userInfos, setUserInfos] = useState([])
+  const [randomUserDataJSON, setrandomUserDataJSON] = useState("");
+
+const fetchNextUser = () =>{
+  fetchapi(nextPageNumber).then((randomData) => {
+    // setrandomUserDataJSON(JSON.stringify(randomData, null, 2) || 'no user data found');
+    const newUserInfos=[
+      ...userInfos,
+      ...randomData.results,
+    ]
+    setUserInfos(randomData.results);
+    setNextPageNumber(randomData.info.page + 1)
+  })
+}
+
+  useEffect(() => {
+    fetchNextUser(nextPageNumber)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Shivam</h1>
+      <h2>There is some magic here..</h2>
+      <p>{counter}</p>
+      <button onClick={() => {
+        setCounter(counter + 1)
+      }}>Increment here</button>
+      <button onClick={()=>{
+        fetchNextUser();
+      }}>fetch next user</button>
+      {
+        userInfos.map((userInfo: UserName, idx: number) => (
+          <div key={idx}>
+            <p>{getFullUsername(userInfo)}</p>
+            <img src={userInfo.picture.thumbnail}></img>
+          </div>
+        ))
+      }
+
+      <p>{randomUserDataJSON}</p>
     </div>
   );
 }
-
-export default App;
